cbaef39c7a7bbe893aefba89f69bc9dc3fa8b35b
Jerome <dengxb19@foxmail.com>
2025-06-02 14:08:11 +0800

    feat: 移除@iconify-icon/react依赖并替换为自定义SVG图标

--- components/login/login-form.tsx
+++ components/login/login-form.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { Icon } from '@iconify-icon/react'
 import { Loader2, Mail } from 'lucide-react'
 import { signIn } from 'next-auth/react'
 import { useTranslations } from 'next-intl'
@@ -64,11 +63,7 @@ export default function LoginForm({ onSuccess }: LoginFormProps) {
         variant="outline"
         className="bg-primary/90 hover:bg-primary w-full py-6"
       >
-        {isLoading.google ? (
-          <Loader2 className="mr-2 h-5 w-5 animate-spin" />
-        ) : (
-          <Icon icon="dashicons:google" width="20" height="20"></Icon>
-        )}
+        {isLoading.google ? <Loader2 className="mr-2 h-5 w-5 animate-spin" /> : <DashiconsGoogle />}
         {t('continueWithGoogle')}
       </Button>
 
@@ -95,5 +90,18 @@ export default function LoginForm({ onSuccess }: LoginFormProps) {
         {error && <div className="text-sm text-red-600">{error}</div>}
       </form>
     </div>
+  )
+}
+
+function DashiconsGoogle() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 20 20">
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M17.6 8.5h-7.5v3h4.4c-.4 2.1-2.3 3.5-4.4 3.4c-2.6-.1-4.6-2.1-4.7-4.7c-.1-2.7 2-5 4.7-5.1c1.1 0 2.2.4 3.1 1.2l2.3-2.2C14.1 2.7 12.1 2 10.2 2c-4.4 0-8 3.6-8 8s3.6 8 8 8c4.6 0 7.7-3.2 7.7-7.8c-.1-.6-.1-1.1-.3-1.7"
+        clipRule="evenodd"
+      ></path>
+    </svg>
   )
 }
